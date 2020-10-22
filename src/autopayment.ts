5d282d2b61a98ab4ba29eb8cb380b4b2750fae69
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-10-22 16:47:13 +0800

    better error messages

--- src/autopayment.ts
+++ src/autopayment.ts
@@ -3,6 +3,8 @@ import { IAutoPaymentOrder, IOrderResponseData } from './types';
 export const autopayment = (SECRET: string) => {
   return async (orderData: IAutoPaymentOrder, log?: boolean): Promise<IOrderResponseData | Error | undefined> => {
     try {
+      let returnData: IOrderResponseData;
+
       // could add check here if recieve address is dev's own
       if (log) console.log('==============orderData==============\n', orderData);
       const orderWithSecret = {
@@ -13,9 +15,17 @@ export const autopayment = (SECRET: string) => {
       const orderResponse = await axios.post('https://www.ddpurse.com/openapi/pay_small_money', orderWithSecret);
       const orderResponseData = orderResponse.data;
       if (log) console.log('==============orderResponseData==============', orderResponseData);
-      if (orderResponseData.code !== 0) throw orderResponse;
+      if (
+        // balance or limit too low
+        orderResponseData.code === -101001 ||
+        orderResponseData.code === -10039 ||
+        orderResponseData.code === -10044
+      ) {
+        returnData = { error: orderResponseData.msg || orderResponseData.data || orderResponseData };
+        return returnData;
+      } else if (orderResponseData.code !== 0) throw orderResponse;
       if (orderResponseData.data) {
-        const returnData: IOrderResponseData = orderResponseData.data;
+        returnData = orderResponseData.data;
         return returnData;
       } else {
         throw orderResponseData;
